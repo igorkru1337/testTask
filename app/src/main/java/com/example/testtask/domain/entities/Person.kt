3a0542de7f13cb935a2980a541778553f6321178
igorkru1337 <krupenni@list.ru>
2021-05-26 22:21:16 +0700

    Создал entity для domain слоя

---
+++ app/src/main/java/com/example/testtask/domain/entities/Person.kt
@@ -0,0 +1,9 @@
+package com.example.testtask.domain.entities
+
+data class Person(
+    val id: Long,
+    val firstName: String,
+    val secondName: String,
+    val birthDate: String,
+    val password: String
+)
